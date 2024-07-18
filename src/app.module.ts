92c43ffeeb1be1823e4ff5418d114f4c78d1815e
Sanusi Hassan <60857931+Sanusihassan@users.noreply.github.com>
2024-07-18 13:52:08 +0000

    connected to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: "mysql",
+    host: "localhost",
+    port: 3306,
+    username: "root",
+    password: "my-secret-pw",
+    database: "nest_js_mysql_tutorial",
+    entities: [],
+    synchronize: true
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
